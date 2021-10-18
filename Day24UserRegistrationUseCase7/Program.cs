04bf0f25bb8442c3be9c382f0bc5d02a2e110a83
Vaibhav Aher1313 <Vaibhav_Aher1313@jabil.com>
2021-10-18 10:49:29 +0530

    ok

---
+++ Day24UserRegistrationUseCase7/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Day21UserRegistrationUseCase7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UseCase7 obj = new UseCase7();
+            obj.insertData();
+        }
+    }
+}
---
+++ Day24UserRegistrationUseCase7/UseCase7.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Day21UserRegistrationUseCase7
+{
+    public class UseCase7
+    {
+        public void insertData()
+        {
+            try
+            {
+                Regex reg = new Regex(@"[0-9]+");
+                Console.WriteLine("Enter Password");
+                string pass = Console.ReadLine();
+                if (reg.IsMatch(pass))
+                    Console.WriteLine("Password : " + pass);
+                else
+                    Console.WriteLine("Must have one numeric value");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+        }
+    }
+}

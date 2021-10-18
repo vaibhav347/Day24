e757927f3fc2dcf2a57e59f6f338ec97f8fe809e
Vaibhav Aher1313 <Vaibhav_Aher1313@jabil.com>
2021-10-18 10:48:40 +0530

    ok

---
+++ Day24UserRegistrationUseCase6/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Day21UserRegistrationUseCase6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UseCase6 obj = new UseCase6();
+            obj.insertData();
+        }
+    }
+}
---
+++ Day24UserRegistrationUseCase6/UseCase6.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Day21UserRegistrationUseCase6
+{
+    public class UseCase6
+    {
+        public void insertData()
+        {
+            try
+            {
+                Regex reg = new Regex(@"[A-Z]+");
+                Console.WriteLine("Enter Password");
+                string pass = Console.ReadLine();
+                if (reg.IsMatch(pass))
+                    Console.WriteLine("Password : " + pass);
+                else
+                    Console.WriteLine("Must have onne upper case character");
+            }catch(Exception e)
+            {
+                Console.WriteLine(e);
+            }
+            
+        }
+    }
+}

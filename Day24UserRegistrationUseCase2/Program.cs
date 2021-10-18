701ee6fc82e61cfd4bbf7aabc1a8667ace68aacd
Vaibhav Aher1313 <Vaibhav_Aher1313@jabil.com>
2021-10-18 10:46:38 +0530

    ok

---
+++ Day24UserRegistrationUseCase2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Day21UserRegistrationUseCase2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UseCase2 obj = new UseCase2();
+            obj.insertData();
+        }
+    }
+}
---
+++ Day24UserRegistrationUseCase2/UseCase2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Day21UserRegistrationUseCase2
+{
+    public class UseCase2
+    {
+        public void insertData()
+        {
+            try
+            {
+                Regex reg = new Regex("^([A-Z]+[A-Za-z])");
+                Console.WriteLine("Enter Last name");
+                string fname = Console.ReadLine();
+                if (reg.IsMatch(fname) && fname.Length >= 3)
+                    Console.WriteLine("Your name is : " + fname);
+                else
+                    Console.WriteLine("Enter Valid Name");
+            }catch(Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+
+        }
+    }
+}

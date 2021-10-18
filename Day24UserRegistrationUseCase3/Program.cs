b96df0f0882ccadda7fca9c4ee404c16e9508af0
Vaibhav Aher1313 <Vaibhav_Aher1313@jabil.com>
2021-10-18 10:47:07 +0530

    ok

---
+++ Day24UserRegistrationUseCase3/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Day21UserRegistrationUseCase3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UseCase3 obj = new UseCase3();
+            obj.insertData();
+        }
+    }
+}
---
+++ Day24UserRegistrationUseCase3/UseCase3.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Day21UserRegistrationUseCase3
+{
+    public class UseCase3
+    {
+        public void insertData()
+        {
+            try
+            {
+                Regex reg = new Regex(@"^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([azA-Z]{2,4}|[0-9]{1,3})(\]?)$");
+                Console.WriteLine("Enter mail-id");
+                string mail = Console.ReadLine();
+                if (reg.IsMatch(mail))
+                    Console.WriteLine("Your Mail : " + mail);
+                else
+                    Console.WriteLine("Enter Valid Mail");
+            }catch(Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}

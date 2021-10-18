ea207252603c9124e1625c4dd7f045bc01f5ae42
Vaibhav Aher1313 <Vaibhav_Aher1313@jabil.com>
2021-10-18 10:51:34 +0530

    ok

---
+++ Day24UserRegistrationUseCase11/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Day24UserRegistrationUseCase11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UseCase11 obj = new UseCase11();
+            obj.insertData("vaibhav@gmail.com");
+            obj.insertData("vaibhavaher347@gmail.com");
+            obj.insertData("vaibhav_aher13jabil.com");
+            obj.insertData("vaibhav@abc.com.in");
+
+        }
+    }
+}
---
+++ Day24UserRegistrationUseCase11/UseCase11.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Day24UserRegistrationUseCase11
+{
+    public class UseCase11
+    {
+        public void insertData(string mail)
+        {
+            try
+            {
+                Regex reg = new Regex(@"^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([azA-Z]{2,4}|[0-9]{1,3})(\]?)$");
+                 if (reg.IsMatch(mail))
+                    Console.WriteLine("Your Mail : " + mail);
+                else
+                    Console.WriteLine("Enter Valid Mail");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+    }
+}

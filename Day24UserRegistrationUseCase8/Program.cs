6f4dd3e4a1333d44f8cb399cd8982d50bfc21a53
Vaibhav Aher1313 <Vaibhav_Aher1313@jabil.com>
2021-10-18 10:49:58 +0530

    ok

---
+++ Day24UserRegistrationUseCase8/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Day21UserRegistrationUseCase8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UseCase8 obj = new UseCase8();
+            obj.insertData();
+        }
+    }
+}
---
+++ Day24UserRegistrationUseCase8/UseCase8.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Day21UserRegistrationUseCase8
+{
+    public class UseCase8
+    {
+        public void insertData()
+        {
+            try
+            {
+                Regex reg = new Regex(@"[#?!@$%^&*-]+");
+                Console.WriteLine("Enter Password");
+                string pass = Console.ReadLine();
+                if (reg.IsMatch(pass))
+                    Console.WriteLine("Password : " + pass);
+                else
+                    Console.WriteLine("Must have one special character");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+        }
+
+    }
+}

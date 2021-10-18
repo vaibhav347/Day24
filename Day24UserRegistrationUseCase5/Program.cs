d30bfc04a8c36f887bf4065621e9e8868494d33b
Vaibhav Aher1313 <Vaibhav_Aher1313@jabil.com>
2021-10-18 10:48:11 +0530

    ok

---
+++ Day24UserRegistrationUseCase5/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Day21UserRegistrationUseCase5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UseCase5 obj = new UseCase5();
+            obj.insertData();
+        }
+    }
+}
---
+++ Day24UserRegistrationUseCase5/UseCase5.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Day21UserRegistrationUseCase5
+{
+    public class UseCase5
+    {
+        public void insertData()
+        {
+            try
+            {
+                Regex reg=new Regex(@".{8,}");
+                Console.WriteLine("Enter Password");
+                string pass = Console.ReadLine();
+                if (reg.IsMatch(pass))
+                    Console.WriteLine("Your password : " + pass);
+                else
+                    Console.WriteLine("Password must contain atleast 8 characters");
+            }catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}

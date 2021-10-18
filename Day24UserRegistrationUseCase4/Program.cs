c0e1ececd50ad41a58c4dfe1f292f1f063d8659c
Vaibhav Aher1313 <Vaibhav_Aher1313@jabil.com>
2021-10-18 10:47:34 +0530

    ok

---
+++ Day24UserRegistrationUseCase4/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Day21UserRegistrationUseCase4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UseCase4 obj = new UseCase4();
+            obj.insertData();
+        }
+    }
+}
---
+++ Day24UserRegistrationUseCase4/UseCase4.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Day21UserRegistrationUseCase4
+{
+    public class UseCase4
+    {
+        public void insertData()
+        {
+            try
+            {
+                Regex reg = new Regex("^91[0-9]{9}");
+                Console.WriteLine("Enter Mobile Number");
+                string mobile = Console.ReadLine();
+                if (reg.IsMatch(mobile))
+                    Console.WriteLine("Mobile : " + mobile);
+                else
+                    Console.WriteLine("Enter Valid Number");
+            }catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+        }
+    }
+}
